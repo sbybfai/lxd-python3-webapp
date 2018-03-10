d642af64b593af5bfd7c2b9b26b1ba7665fb89df
sbybfai <sbybfai@gmail.com>
2018-03-10 16:05:24 +0800

    日志内容使用插件editor.md,支持可视化操作。

--- www/handlers.py
+++ www/handlers.py
@@ -324,7 +324,6 @@ async def api_blogs(*, page='1'):
 	blogs = await Blog.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
 	return dict(page=p, blogs=blogs)
 
-
 @post('/api/blogs/{id}/delete')
 async def api_delete_blog(request, *, id):
 	check_admin(request)
